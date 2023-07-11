db044d9652ad9b24c3d28c30637ccd4c886fc908
Binkeurss <nvbinh271104@gmail.com>
2023-07-12 01:56:08 +0700

    Bink

--- VT11/VT11.cpp
+++ VT11/VT11.cpp
@@ -1,20 +1,44 @@
-// VT11.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include<iostream>
+#include<sstream>
+#include<iomanip>
+#include<vector>
+using namespace std;
 
-#include <iostream>
+void Nhap(vector<int>&, int&);
+string SapXep(vector<int>&);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int n;
+	vector<int> v;
+	Nhap(v, n);
+	cout << SapXep(v);
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void Nhap(vector<int>& v, int& n)
+{
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+		cin >> x;
+		v.push_back(x);
+	}
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+string SapXep(vector<int>& v)
+{
+	stringstream stream;
+	for (int i = 1; i < v.size() - 1; i++)
+	{
+		int index = i;
+		for (int j = i + 1; j < v.size() - 1; j++)
+			if (v[j] < v[index])
+				index = j;
+		swap(v[i], v[index]);
+	}
+	for (int i = 0; i < v.size(); i++)
+		stream << setw(12) << v[i];
+	return stream.str();
+}
