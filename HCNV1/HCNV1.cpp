f317f00ebe19a3c985d16448a0cff78948b6febf
Binkeurss <nvbinh271104@gmail.com>
2023-07-12 16:17:09 +0700

    Bink

--- HCNV1/HCNV1.cpp
+++ HCNV1/HCNV1.cpp
@@ -1,20 +1,24 @@
-// HCNV1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include<string>
+#include<sstream>
+using namespace std;
+
+string ktBenTrong(int, int, int, int, int, int);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int x1, y1, x2, y2, x, y;
+    cin >> x1 >> y1 >> x2 >> y2 >> x >> y;
+    cout << ktBenTrong(x1, y1, x2, y2, x, y);
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+string ktBenTrong(int x1, int y1, int x2, int y2, int x, int y)
+{
+    stringstream stream;
+    if (min(x1, x2) < x && x < max(x1, x2) && min(y1, y2) < y && y < max(y1, y2))
+        stream << "YES";
+    else
+        stream << "NO";
+    return stream.str();
+}
